8c463736f952f040ac3235d1783ae651c4062b00
Ariston <aristidischatzidis@gmail.com>
2024-06-26 17:06:56 +0300

    Creating User, Booking and room entities.

---
+++ src/main/java/entity/Booking.java
@@ -0,0 +1,23 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Room room;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    //Getters Setters
+}
---
+++ src/main/java/entity/Room.java
@@ -0,0 +1,19 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomNumber;
+    private String roomType;
+    private Double roomPrice;
+    private String status;
+
+    //getters setters
+}
